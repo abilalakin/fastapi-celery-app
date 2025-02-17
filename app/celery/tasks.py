38a4873ab3485f0e3af9848e2c4db1f45af487bc
abilalakin <abilalakin@gmail.com>
2025-02-17 05:30:41 +0100

    Add typing to task functions

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -1,7 +1,9 @@
 import logging
 import httpx
+import uuid
 from celery.exceptions import Ignore
 from asgiref.sync import async_to_sync
+from typing import Any, Tuple
 from app.celery_app import celery_app
 from ..config import settings
 from ..database import AsyncSessionLocal
@@ -11,7 +13,7 @@ from ..models.job import Job
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-async def update_job_status(job_id, status, result=None) -> Job:
+async def update_job_status(job_id: uuid.UUID, status: str, result: Any = None) -> Job:
     async with AsyncSessionLocal() as session:
         job = await session.get(Job, job_id)
         job.status = status
@@ -20,14 +22,14 @@ async def update_job_status(job_id, status, result=None) -> Job:
         await session.commit()
         return job
                 
-def handle_task_exception(job_id, exc):
+def handle_task_exception(job_id: uuid.UUID, exc: Exception) -> None:
     logger.error(f"Job {job_id} failed: {exc}")
     async_to_sync(update_job_status)(job_id, "error", str(exc))
     raise Ignore()
 
 
 @celery_app.task(bind=True, name="app.celery.tasks.step_a")
-def step_a(self, job_id):
+def step_a(self, job_id: uuid.UUID) -> Tuple[uuid.UUID, Any]:
     try:
         logger.info(f"Starting step_a with job_id: {job_id}")
         with httpx.Client() as client:
@@ -42,7 +44,7 @@ def step_a(self, job_id):
 
 
 @celery_app.task(bind=True, name="app.celery.tasks.step_b")
-def step_b(self, payload):
+def step_b(self, payload: Tuple[uuid.UUID, Any]) -> uuid.UUID:
         try:
             job_id, data = payload
             logger.info(f"Starting step_b with job_id: {job_id}")
@@ -54,7 +56,7 @@ def step_b(self, payload):
 
 
 @celery_app.task(bind=True, name="app.celery.tasks.step_c")
-def step_c(self, job_id):
+def step_c(self, job_id: uuid.UUID) -> Any:
         try:
             logger.info(f"Starting step_c with job_id: {job_id}")
             # Update job status to "completed"
